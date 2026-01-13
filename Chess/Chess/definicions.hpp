cbc7406bd8f7f38e3443e92812e8fa3bb6fffdca
Jofre <jofre.puig@estudiant.enti.cat>
2026-01-13 18:53:24 +0100

    Regles2.hpp

--- Chess/Chess/definicions.hpp
+++ Chess/Chess/definicions.hpp
@@ -14,4 +14,4 @@ struct Jugador {
 // Constants de peces
 const char REI_B = 'K', REINA_B = 'Q', TORRE_B = 'R', ALFIL_B = 'B', CAVALL_B = 'N', PEO_B = 'P';
 const char REI_N = 'k', REINA_N = 'q', TORRE_N = 'r', ALFIL_N = 'b', CAVALL_N = 'n', PEO_N = 'p';
-const char CASELLA_BUIDA = '*';
+const char CASELLA_BUIDA = '*';//h
