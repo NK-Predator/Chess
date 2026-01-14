89943a626bfc0e753b703f84ad81d34979a5565d
Jofre <jofre.puig@estudiant.enti.cat>
2026-01-14 03:24:44 +0100

    MAIN

--- Chess/Chess/Main.cpp
+++ Chess/Chess/Main.cpp
@@ -1,43 +1,7 @@
 #include <iostream>
-#include <string>
-#include <locale.h>
-#include <list>
-#include <stdlib.h>
-#include <vector>
-#include <time.h>
-
-char taulell[8][8];
-
-void createTaulell() {
-
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (i > 1 && i < 6) {
-				taulell[i][j] = '*';
-			}
-			else {
-				taulell[i][j] = ' ';
-			}
-		}
-		std::cout << std::endl;
-	}
-}
-
-void printTaulell() {
-	std::cout << "  1 2 3 4 5 6 7 8\n";
-	for (int i = 0; i < 8; ++i) {
-		std::cout << 8 - i << ' ';          
-		for (int j = 0; j < 8; ++j) {
-			std::cout << taulell[i][j] << ' ';
-		}
-		std::cout << '\n';
-	}
-
-}
-
+#include "joc.hpp"
 
 int main() {
-	createTaulell();
-	printTaulell();
-	return 0;
+    jugarPartida();
+    return 69;
 }
